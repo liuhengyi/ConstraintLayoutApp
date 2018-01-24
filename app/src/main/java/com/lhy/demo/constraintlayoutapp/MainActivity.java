1b12d5f2471540861a3b627fc74c8aa4417da366
liuhengyi <408873967@qq.com>
2018-01-24 15:46:16 +0800

    textview

--- app/src/main/java/com/lhy/demo/constraintlayoutapp/MainActivity.java
+++ app/src/main/java/com/lhy/demo/constraintlayoutapp/MainActivity.java
@@ -2,12 +2,20 @@ package com.lhy.demo.constraintlayoutapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        textView = (TextView) findViewById(R.id.textView);
     }
 }
