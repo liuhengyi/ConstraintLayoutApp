32d44dfd9d11c2702214426f30eb03e1418bf645
liuhengyi <408873967@qq.com>
2018-01-25 10:02:17 +0800

    textview

--- app/src/main/java/com/lhy/demo/constraintlayoutapp/MainActivity.java
+++ app/src/main/java/com/lhy/demo/constraintlayoutapp/MainActivity.java
@@ -1,21 +1,46 @@
 package com.lhy.demo.constraintlayoutapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView textView;
+    @Bind(R.id.textView)
+    TextView textView;
+    @Bind(R.id.button)
+    Button button;
+    @Bind(R.id.activity_main)
+    ConstraintLayout activityMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
         initView();
     }
 
     private void initView() {
-        textView = (TextView) findViewById(R.id.textView);
+
+    }
+
+    @OnClick({R.id.textView, R.id.button})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.textView:
+                break;
+            case R.id.button:
+                Toast.makeText(this, "tusi", Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
